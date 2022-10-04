b7af4955f63beb316ecafb746622ad5148389436
andreiBrbsh <andrei.brbsh@icloud.com>
2022-10-04 18:24:28 +0300

    add changed item function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 'use sctrict';
 
+const items = document.querySelectorAll('.cards-container__item');
+
+for(let item of items){
+      item.addEventListener('click', function(){
+            item.classList.toggle('cards-container__item_changed');
+      })      
+}
 
